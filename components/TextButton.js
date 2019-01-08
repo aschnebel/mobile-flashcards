c9e25db317aea05ca37b92e5188c5a6f75ef7acc
Alexander Schnebel <schnebel.alexander@gmail.com>
2019-01-08 10:26:28 +0100

    refactor: Made TextButton even more customizable

--- components/TextButton.js
+++ components/TextButton.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import { Text, TouchableOpacity, StyleSheet } from 'react-native'
 
-import { black } from '../utils/colors';
+import { black } from '../utils/colors'
 
-export default TextButton = ({ children, onPress, style = {} }) => (
-  <TouchableOpacity onPress={onPress}>
-    <Text style={[styles.btn, style]}>{children}</Text>
+export default (TextButton = ({ children, onPress, style = {}, textStyle = {} }) => (
+  <TouchableOpacity style={[styles.button, style]} onPress={onPress}>
+    <Text style={[styles.buttonText, textStyle]}>{children}</Text>
   </TouchableOpacity>
-)
+))
 
 const styles = StyleSheet.create({
-  btn: {
+  button: {
     padding: 15,
-    paddingLeft: 30,
-    paddingRight: 30,
-    alignSelf: 'center',
+    paddingLeft: 50,
+    paddingRight: 50,
     borderRadius: 5,
-    margin: 20,
+    margin: 5,
     borderWidth: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
     fontSize: 16
   }
 })
